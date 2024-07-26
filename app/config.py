7775754f4978328be926bbe2359ff0db1ff8fe8b
Tyler-Gorshing <tyler.gorshing@clevyr.com>
2024-07-26 13:15:11 -0500

    feat: add new configurations

--- app/config.py
+++ app/config.py
@@ -4,3 +4,7 @@ import os
 # this default url will not work
 ollama_base_url = os.getenv('OLLAMA_BASE_URL', 'http://localhost:11434/')
 ollama_model = os.getenv('OLLAMA_MODEL','mistral')
+
+openai_model = os.getenv('OPENAI_MODEL')
+openai_base_url = os.getenv('OPENAI_BASE_URL')
+openai_api_key = os.getenv('OPENAI_API_KEY')
