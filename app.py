3de091678bf9a9d429a8222cc863210d378450f2
Tyler-Gorshing <tyler.gorshing@clevyr.com>
2024-07-26 13:16:14 -0500

    featL: run agents example

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 # have to load env before importing the app
 load_dotenv()
 
-from app import basic_chain
+from app import basic_chain, basic_agent
 
 
 if __name__ == '__main__':
     basic_chain()
+    basic_agent()
